be4ebdb016ea9fbb26f85003ecce5a67674448ca
Pierre LEVEQUE <pleveque@e3r8p17.42.fr>
2021-07-21 18:25:39 +0200

    try 2

--- ex01/ft_map.c
+++ ex01/ft_map.c
@@ -6,19 +6,25 @@
 /*   By: pleveque <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 15:12:57 by pleveque          #+#    #+#             */
-/*   Updated: 2021/07/19 15:17:49 by pleveque         ###   ########.fr       */
+/*   Updated: 2021/07/21 18:23:39 by pleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "stdlib.h"
 
 int	*ft_map(int *tab, int length, int(*f)(int))
 {
 	int	i;
+	int	*new_tab;
 
+	new_tab = malloc(sizeof(int) * length);
+	if (new_tab == NULL)
+		return (NULL);
 	i = 0;
 	while (i < length)
 	{
-		tab[i] = f(tab[i]);
+		new_tab[i] = f(tab[i]);
 		i++;
 	}
-	return (tab);
+	return (new_tab);
 }
